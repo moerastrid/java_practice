6567da9b54e9f562ea34461d5974d5efe3f10869
moerastrid@gmail.com <moerastrid@gmail.com>
2024-09-17 12:16:47 +0200

    compile a file

---
+++ tutorial/Test.java
@@ -0,0 +1,19 @@
+// class Test {
+// 	public static void main(String [] args) {
+// 		System.out.println("hello world!");
+// 	}
+// }
+
+class A {
+	public static void main( String [] args )
+	{
+		System.out.println("aaa Hello world!");
+	}
+}
+
+class B {
+	public static void main( String [] args )
+	{
+		System.out.println("bbb Hello world!");
+	}
+}
