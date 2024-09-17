f802e9cf010c12c2af69e5da10d0251481aa469c
moerastrid@gmail.com <moerastrid@gmail.com>
2024-09-17 15:19:48 +0200

    did not see 6, but done now

--- tutorial/lesson 1/LastExercise.java
+++ tutorial/lesson 1/LastExercise.java
@@ -1,8 +1,19 @@
 public class LastExercise{
 	public static void main(String [] args) {
-		int [] A = new int[20];
-		int [] B = new int[10];
-		A = B;
-		A[15] = 0;
+		char c = 97;
+		int k = (int)5.6;
+		long x = (long)5.4;
+		short n = (short)99999;
+		short s = (short)-10.0;
+		float y = c;
+		double d = c;
+
+		System.out.println(c);
+		System.out.println(k);
+		System.out.println(x);
+		System.out.println(n);
+		System.out.println(s);
+		System.out.println(y);
+		System.out.println(d);
 	}
 }
